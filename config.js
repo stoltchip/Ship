ca67aac4a7c2c9db437150ded5389ab0add1705f
stoltchip <kabanekna102@gmail.com>
2025-11-05 19:16:43 +0100

    Delete config.js

--- config.js
+++
@@ -1,3 +0,0 @@
-// Auto-generated 2025-11-05T17:51:46.257622Z
-const API_URL = "https://script.google.com/macros/s/AKfycbwf6VgMXPOTD1a7TVGHsw_qYI3t4X8RIwWwaHq1M_ThF4zrPaGL7SmZ9EtgAQBZmyOV/exec";
-const ADMIN_TOKEN = "STOLT-12345";
